055a8ec7431712561f4579ff119fba12ecb41faf
wrongangel <wrongangel@gmail.com>
2023-09-04 14:29:10 +0300

    Setting up linter

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const ESLintPlugin = require('eslint-webpack-plugin')
+
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    if (dev) { // Add this plugin only in dev mode
+      config.plugins.push(
+        new ESLintPlugin({
+          context: './', // Location where it will scan all the files
+          extensions: ['js', 'jsx', 'ts', 'tsx'], // File formats that should be scanned
+          exclude: ['node_modules', 'dist'], // Exclude everything in these folders
+        })
+      )
+    }
+    return config
+  },
+}
 
 module.exports = nextConfig

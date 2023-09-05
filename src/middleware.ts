1705c184ae6f2a39267c178f826776349eec2113
wrongangel <wrongangel@gmail.com>
2023-09-05 18:36:56 +0300

    redirect to login

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export async function middleware (req: NextRequest): Promise<NextResponse> {
   const supabase = createMiddlewareClient<Database>({ req, res })
   const { data: { session } } = await supabase.auth.getSession()
   if (req.nextUrl.pathname.startsWith('/application') && session === null) {
-    return NextResponse.redirect(new URL('/', req.url))
+    return NextResponse.redirect(new URL('/login', req.url))
   }
   return res
 }

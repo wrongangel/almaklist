adc73cab7766804a28b449e85e53b77f7e6c98fc
wrongangel <wrongangel@gmail.com>
2023-10-03 13:15:16 +0300

    Collections card styles

--- src/app/application/dashboard/page.tsx
+++ src/app/application/dashboard/page.tsx
@@ -1,12 +1,13 @@
 import CollectionsList from '@/components/Lists/CollectionsList/CollectionsList'
 import AddList from '@/components/forms/AddList/AddList'
+import styles from './page.module.scss'
 
 const Dashboard = async (): Promise<JSX.Element> => {
   return (
-    <>
+    <div className={styles.dashboard}>
       <CollectionsList />
       <AddList />
-    </>
+    </div>
   )
 }
 export default Dashboard
--- src/components/Cards/CollectionCard/CollectionCard.tsx
+++ src/components/Cards/CollectionCard/CollectionCard.tsx
@@ -2,6 +2,7 @@ import type Collection from '@/models/collection'
 import BasicCard from '../BasicCard/BasicCard'
 import Link from 'next/link'
 import { useCollestionsStore } from '@/stores/collectionsStore'
+import styles from './CollectionCard.module.scss'
 
 interface Props {
   list: Collection
@@ -11,14 +12,12 @@ const CollectionCard = ({ list }: Props): JSX.Element => {
   const collectionsStore = useCollestionsStore()
   return (
     <>
-      <Link href={`/application/list/${list.id}`} key={list.id}>
-        <BasicCard>
-          <>
-            {list.name}
-          </>
-        </BasicCard>
-      </Link>
-      <button onClick={() => { void collectionsStore.removeList(list.id) }}>Delete</button>
+      <BasicCard className={styles.collectionCard}>
+        <Link href={`/application/list/${list.id}`} key={list.id}>
+          {list.name}
+        </Link>
+        <button onClick={() => { void collectionsStore.removeList(list.id) }}>Delete</button>
+      </BasicCard>
     </>
   )
 }

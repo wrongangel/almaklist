4684172f0642ac5e1214c70f11cea208b16e0285
wrongangel <wrongangel@gmail.com>
2023-10-03 12:47:42 +0300

    Logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,13 @@ import Link from 'next/link'
 import styles from './Header.module.scss'
 import Container from '@/components/Container/Container'
 import HeaderRightPart from './HeaderRightPart/HeaderRightPart'
+import LogoIcon from '@/assets/Logo.svg'
+import Image from 'next/image'
 
 const Header = async (): Promise<JSX.Element> => {
   return (
     <Container className={styles.header}>
-      <div className={styles.header__logo}><Link href='/'>Almaklist</Link></div>
+      <div className={styles.header__logo}><Link href='/'><Image src={LogoIcon} alt='logo' /></Link></div>
       <nav className={styles.header__navigation}>
         <ul>
           <li><Link href='#'>changelog</Link></li>
